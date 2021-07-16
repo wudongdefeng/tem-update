f1315c32be3d56545a7d18f41d4d5b296fcecf2b
wudongdefeng <32961310+wudongdefeng@users.noreply.github.com>
2021-07-16 09:39:56 +0800

    diy仓库更新于2021-07-16 09:38:42

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -148,7 +148,7 @@
 #监控crazyJoy分红
 10 12 * * * node /scripts/jd_crazy_joy_bonus.js >> /scripts/logs/jd_crazy_joy_bonus.log 2>&1
 #京喜财富岛
-18 0-23/1 * * * node /scripts/jd_wind0_wind_cfd.js >> /scripts/logs/jd_wind0_wind_cfd.log 2>&1
+18 0-23/1 * * * node /scripts/jd_smiek_gua_wealth_island.js >> /scripts/logs/jd_smiek_gua_wealth_island.log 2>&1
 # 删除优惠券(默认注释，如需要自己开启，如有误删，已删除的券可以在回收站中还原，慎用)
 #20 9 * * 6 node /scripts/jd_delCoupon.js >> /scripts/logs/jd_delCoupon.log 2>&1
 #家庭号
