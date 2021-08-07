98201a0d2c72d6cb669d74c16d56c8bd77132080
wudongdefeng <32961310+wudongdefeng@users.noreply.github.com>
2021-08-07 20:58:11 +0800

    diy仓库更新于2021-08-07 20:55:15

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -70,6 +70,7 @@
 6 0,23 * * * node /scripts/jd_club_lottery.js >> /scripts/logs/jd_club_lottery.log 2>&1
 # 东东农场
 15 6-18/6 * * * node /scripts/jd_fruit.js >> /scripts/logs/jd_fruit.log 2>&1
+36 20 * * * node /scripts/zero_fruit.js >> /scripts/logs/zero_fruit.log 2>&1
 # 东东乐园
 30 7 * * * node /scripts/jd_wen_wind_ddnc_farmpark.js >> /scripts/logs/jd_wen_wind_ddnc_farmpark.log 2>&1
 # 宠汪汪
