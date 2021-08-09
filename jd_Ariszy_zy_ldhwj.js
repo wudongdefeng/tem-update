60bbf67165f8a8701bd3518e38b66694e6ddc3e3
wudongdefeng <32961310+wudongdefeng@users.noreply.github.com>
2021-08-10 00:25:30 +0800

    diy仓库更新于2021-08-10 00:22:35

--- jd_Ariszy_zy_ldhwj.js
+++ jd_Ariszy_zy_ldhwj.js
@@ -183,11 +183,11 @@ async function getLottery(){
     try{
         const result = JSON.parse(data)
         if(logs)$.log(data)
-        if(result.data.bizCode == 0 && result.data.result.userAwardsCacheDto.jBeanAwardVo.prizeName){
+        if(result.data.bizCode == 0 && result.data.result.lotteryReturnCode == 0){
            console.log("\n获得"+result.data.result.userAwardsCacheDto.jBeanAwardVo.prizeName+"\n")
    await $.wait(4000)
         }else{
-           $.log(result.data.bizMsg+"\n")
+           $.log("恭喜你，抽中了0豆豆\n")
         }
         }catch(e) {
           $.logErr(e, response);
