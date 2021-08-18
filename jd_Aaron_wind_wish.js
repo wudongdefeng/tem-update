ee544db6aea91404df1d0106271154eae6066ee6
wudongdefeng <32961310+wudongdefeng@users.noreply.github.com>
2021-08-19 01:43:01 +0800

    diy仓库更新于:2021-08-19 01:43

--- jd_Aaron_wind_wish.js
+++ jd_Aaron_wind_wish.js
@@ -5,17 +5,17 @@
 ===============Quantumultx===============
 [task_local]
 #众筹许愿池
-40 0,20 * * * https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_wish.js, tag=众筹许愿池, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
+40 0,2 * * * https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_wish.js, tag=众筹许愿池, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
 
 ================Loon==============
 [Script]
-cron "40 0,20 * * *" script-path=https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_wish.js,tag=众筹许愿池
+cron "40 0,2 * * *" script-path=https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_wish.js,tag=众筹许愿池
 
 ===============Surge=================
-众筹许愿池 = type=cron,cronexp="40 0,20 * * *",wake-system=1,timeout=3600,script-path=https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_wish.js
+众筹许愿池 = type=cron,cronexp="40 0,2 * * *",wake-system=1,timeout=3600,script-path=https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_wish.js
 
 ============小火箭=========
-众筹许愿池 = type=cron,script-path=https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_wish.js, cronexpr="40 0,20 * * *", timeout=3600, enable=true
+众筹许愿池 = type=cron,script-path=https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_wish.js, cronexpr="40 0,2 * * *", timeout=3600, enable=true
  */
 const $ = new Env('众筹许愿池');
 const notify = $.isNode() ? require('./sendNotify') : '';
@@ -164,7 +164,7 @@ async function healthyDay_getHomeData(type = true) {
                       }
                     }
                   } else if (vo.taskType === 14) {
-                    console.log(`您的助力码：${vo.assistTaskDetailVo.taskToken}`)
+                    console.log(`【京东账号${$.index}（${$.UserName}）的众筹许愿池好友互助码】${vo.assistTaskDetailVo.taskToken}\n`)
                     if (vo.times !== vo.maxTimes) {
                       $.shareCode.push({
                         "code": vo.assistTaskDetailVo.taskToken,
@@ -173,7 +173,7 @@ async function healthyDay_getHomeData(type = true) {
                     }
                   }
                 } else {
-                  console.log(`【${vo.taskName}】已完成`)
+                  console.log(`【${vo.taskName}】已完成\n`)
                 }
               }
             }
@@ -201,7 +201,7 @@ function harmony_collectScore(body = {}, taskType = '') {
               if (taskType === 13) {
                 console.log(`签到成功：获得${data.data.result.score}金币\n`)
               } else if (body.taskId == 6) {
-                console.log(`助力成功：获得${data.data.result.score}金币\n`)
+                console.log(`助力成功：您的好友获得${data.data.result.score}金币\n`)
               } else {
                 console.log(`完成任务：获得${data.data.result.score}金币\n`)
               }
@@ -237,8 +237,8 @@ function interact_template_getLotteryResult() {
           if (safeGet(data)) {
             data = JSON.parse(data);
             let userAwardsCacheDto = data.data.result.userAwardsCacheDto
-            if (userAwardsCacheDto.type === 2) {
-              console.log(`抽中${userAwardsCacheDto.jBeanAwardVo.quantity}${userAwardsCacheDto.jBeanAwardVo.ext}`)
+            if (userAwardsCacheDto && userAwardsCacheDto.type === 2) {
+              console.log(`抽中：${userAwardsCacheDto.jBeanAwardVo.quantity}${userAwardsCacheDto.jBeanAwardVo.ext}`)
             } else {
               console.log(JSON.stringify(data))
             }
