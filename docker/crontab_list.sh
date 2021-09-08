a1ec59754faf9a44027672fecab3a70ca26322e7
wudongdefeng <32961310+wudongdefeng@users.noreply.github.com>
2021-09-08 15:39:54 +0800

    diy仓库更新于:2021-09-08 15:37

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -67,7 +67,7 @@
 #玩一玩成就
 48 11 * * * node /scripts/jd_tsuk_wind_wyw.js >> /scripts/logs/jd_tsuk_wind_wyw.log 2>&1
 #京东试用
-15 12 * * * node /scripts/jd_imwcc_wind_try.js >> /scripts/logs/jd_imwcc_wind_try.log 2>&1
+15 12 * * * node /scripts/jd_try.js >> /scripts/logs/jd_try.log 2>&1
 #图形签到
 20 5 * * * node /scripts/jd_smiek_wind_sign_graphics.js >> /scripts/logs/jd_smiek_wind_sign_graphics.log 2>&1
 # 签到
