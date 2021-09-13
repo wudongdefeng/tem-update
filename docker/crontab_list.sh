d93ca63cf8cab595e37fc24695aa4684847889e4
wudongdefeng <32961310+wudongdefeng@users.noreply.github.com>
2021-09-13 13:39:32 +0800

    diy仓库更新于:2021-09-13 13:37

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -4,6 +4,8 @@
 30 * * * * sh +x /scripts/docker/auto_help.sh collect >> /scripts/logs/auto_help_collect.log 2>&1
 
 ##############短期活动##############
+# 希捷品牌日
+17 10 * 9,10 * node /scripts/jd_smiek_gua_UnknownTask4.js >> /scripts/logs/jd_smiek_gua_UnknownTask4.log 2>&1
 #开学充电站
 30 1 * * * node /scripts/jd_Ariszy_zy_kxcdz.js >> /scripts/logs/jd_Ariszy_zy_kxcdz.log 2>&1
 #内容鉴赏官
