89cdac7bcf51aab3846e7a916025b4b427b87144
wudongdefeng <32961310+wudongdefeng@users.noreply.github.com>
2021-09-18 14:33:31 +0800

    diy仓库更新于:2021-09-18 14:30

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -4,6 +4,8 @@
 30 * * * * sh +x /scripts/docker/auto_help.sh collect >> /scripts/logs/auto_help_collect.log 2>&1
 
 ##############短期活动##############
+# 手机狂欢城
+0 0-18/6 * * * node /scripts/jd_carnivalcity.js >> /scripts/logs/jd_carnivalcity.log 2>&1
 # 许愿池
 40 0,2 * * * node /scripts/jd_wish.js >> /scripts/logs/jd_wish.log 2>&1
 # 企有此礼
