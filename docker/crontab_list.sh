fc2aa219bb0a06a5bd0270ac1752c22c5b7cb8a0
wudongdefeng <32961310+wudongdefeng@users.noreply.github.com>
2021-11-02 16:46:30 +0800

    diy仓库更新于:2021-11-02 16:43

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -58,7 +58,7 @@
 #汪汪乐园
 11 12 * * * node /scripts/jd_wind_joy_park.js >> /scripts/logs/jd_wind_joy_park.log 2>&1
 # 特物
-21 5,12 * * * node /scripts/jd_superBrand.js >> /scripts/logs/jd_superBrand.log 2>&1
+21 5,12 * * * node /scripts/jd_star_wind_superBrand.js >> /scripts/logs/jd_star_wind_superBrand.log 2>&1
 #超级直播间红包雨(活动时间不定期，出现异常提示请忽略。红包雨期间会正常)
 1,31 0-23/1 * * * node /scripts/jd_live_redrain.js >> /scripts/logs/jd_live_redrain.log 2>&1
 #粉丝互动
