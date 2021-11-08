dd3f1a44725110f1fbfc033c161011c0a6c5fedf
wudongdefeng <32961310+wudongdefeng@users.noreply.github.com>
2021-11-08 19:00:45 +0800

    diy仓库更新于:2021-11-08 18:57

--- jd_jxmc.js
+++ jd_jxmc.js
@@ -103,11 +103,11 @@ if ($.isNode()) {
     await pasture();
     await $.wait(2000);
   }
-  $.res = await getAuthorShareCode('https://code.aliyun.com/wudongdefeng/updateteam/raw/master/shareCodes/jxmc.txt')
+  $.res = await getAuthorShareCode('https://code.aliyun.com/wudongdefeng/updateteam/raw/master/shareCodes/jxmc.json')
   if (!$.res) {
-    $.http.get({url: 'https://code.aliyun.com/wudongdefeng/updateteam/raw/master/shareCodes/jxmc.txt'}).then((resp) => {}).catch((e) => console.log('刷新CDN异常', e));
+    $.http.get({url: 'https://code.aliyun.com/wudongdefeng/updateteam/raw/master/shareCodes/jxmc.json'}).then((resp) => {}).catch((e) => console.log('刷新CDN异常', e));
     await $.wait(1000)
-    $.res = await getAuthorShareCode('https://code.aliyun.com/wudongdefeng/updateteam/raw/master/shareCodes/jxmc.txt')
+    $.res = await getAuthorShareCode('https://code.aliyun.com/wudongdefeng/updateteam/raw/master/shareCodes/jxmc.json')
   }
   await shareCodesFormat()
   for (let i = 0; i < cookiesArr.length; i++) {
