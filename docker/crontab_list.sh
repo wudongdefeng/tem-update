601cd7175e6b888fc4b4ea42581ad5e0c548f8ad
wudongdefeng <32961310+wudongdefeng@users.noreply.github.com>
2021-11-25 10:15:04 +0800

    diy仓库更新于:2021-11-25 10:12

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -92,8 +92,6 @@
 
 #5G超级盲盒(活动时间：2021-06-2到2021-07-31)
 0 0-23/4 * * * node /scripts/jd_mohe.js >> /scripts/logs/jd_mohe.log 2>&1
-#金榜创造营 活动时间：2021-05-21至2021-12-31
-0 1,22 * * * node /scripts/jd_gold_creator.js >> /scripts/logs/jd_gold_creator.log 2>&1
 ##############长期活动##############
 
 # 领卷中心签到
