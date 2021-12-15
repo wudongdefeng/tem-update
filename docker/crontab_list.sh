9abe610325812208cc237e94af77007531ed2e42
wudongdefeng <32961310+wudongdefeng@users.noreply.github.com>
2021-12-15 16:48:18 +0800

    diy仓库更新于:2021-12-15 16:45

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -7,8 +7,6 @@
 
 # 我的理想家
 3 1,8 * * * node /scripts/jd_xiao_wind_lxLottery.js >> /scripts/logs/jd_xiao_wind_lxLottery.log 2>&1
-# 金榜年终奖
-3 2,10 * * * node /scripts/jd_split.js >> /scripts/logs/jd_split.log 2>&1
 # 金榜
 33 0,22 * * * node /scripts/jd_gold_creator.js >> /scripts/logs/jd_gold_creator.log 2>&1
 # 饭粒
