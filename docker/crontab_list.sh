8f7576e4c31d853bb12f3eeee76e5c7738f545f0
wudongdefeng <32961310+wudongdefeng@users.noreply.github.com>
2022-01-04 21:04:59 +0800

    diy仓库更新于:2022-01-04 21:01

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -55,8 +55,6 @@
 
 # 京喜购物返红包
 44 */6 * * * node /scripts/jd_air_wind_aid_cashback.js >> /scripts/logs/jd_air_wind_aid_cashback.log 2>&1
-# 京喜领88元红包
-4 2,10 * * * node /scripts/jd_jxlhb.js >> /scripts/logs/jd_jxlhb.log 2>&1
 # 女装盲盒
 4 2,10 * * * node /scripts/jd_nzmh.js >> /scripts/logs/jd_nzmh.log 2>&1
 
