277518ebb3e5db423dbd98a7f483c9de74571f69
wudongdefeng <32961310+wudongdefeng@users.noreply.github.com>
2022-01-17 10:41:14 +0800

    diy仓库更新于:2022-01-17 10:38

--- jd_opencardL48.js
+++ jd_opencardL48.js
@@ -55,7 +55,7 @@ let activityCookie =''
   $.userId = '10299171'
   $.actId = '897353f1fe1a4958a48_220117'
   $.MixNicks = ''
-  $.inviteNick = 'k1Nobb+P0er+C2sysxnx/P2KELO9izRVpwCyqu0eqVZ5aW7RHzlMobrzJ/e9r/uf'
+  $.inviteNick = 'clgbkabWGOqxDYqcXo0rh2BUh0gimxufjFRMgNwy0cYiU+N6ZQy0LI2KZrs00RAY'
   console.log(`请自行测试有水无水。`)
   for (let i = 0; i < cookiesArr.length; i++) {
     cookie = cookiesArr[i];
