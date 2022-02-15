e370069ef6518d138a8a4e355f10281165f27b57
wudongdefeng <32961310+wudongdefeng@users.noreply.github.com>
2022-02-15 22:40:43 +0800

    diy仓库更新于:2022-02-15 22:37

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -124,7 +124,7 @@
 # 东东工厂
 26 * * * * node /scripts/jd_jdfactory.js >> /scripts/logs/jd_jdfactory.log 2>&1
 # 赚京豆(微信小程序)
-12 0,9 * * * node /scripts/jd_wind_syj_Mod.js >> /scripts/logs/jd_wind_syj_Mod.log 2>&1
+12 0,9 * * * node /scripts/jd_faker_wind_syj.js >> /scripts/logs/jd_faker_wind_syj 2>&1
 # 送豆得豆(京东赚赚)
 47 2,13 * * * node /scripts/jd_sendBeans.js >> /scripts/logs/jd_sendBeans.log 2>&1
 # 京东快递签到
