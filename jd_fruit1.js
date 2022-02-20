2e18fcbc54bf67464e3a40b639b7cdc08ab2daa1
wudongdefeng <32961310+wudongdefeng@users.noreply.github.com>
2022-02-20 18:17:03 +0800

    diy仓库更新于:2022-02-20 18:14

--- jd_fruit1.js
+++ jd_fruit1.js
@@ -1368,8 +1368,8 @@ function requireConfig() {
   return new Promise(resolve => {
     console.log('开始获取配置文件\n')
     notify = $.isNode() ? require('./sendNotify') : '';
-    //Node.js用户请在jdCookie.js处填写京东ck;
-    const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
+    //Node.js用户请在jdCookie1.js处填写京东ck;
+    const jdCookieNode = $.isNode() ? require('./jdCookie1.js') : '';
     const jdFruitShareCodes = $.isNode() ? require('./jdFruitShareCodes.js') : '';
     //IOS等用户直接用NobyDa的jd cookie
     if ($.isNode()) {
