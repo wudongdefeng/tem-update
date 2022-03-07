a41c3589c4c6eb13f3099ec099c8455f18f3cf45
wudongdefeng <32961310+wudongdefeng@users.noreply.github.com>
2022-03-08 02:48:27 +0800

    diy仓库更新于:2022-03-08 02:45

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -174,8 +174,7 @@
 #京东极速版签到+赚现金任务
 21 1,6 * * * node /scripts/jd_speed_sign.js >> /scripts/logs/jd_speed_sign.log 2>&1
 #京喜财富岛
-18 0-23/1 * * * node /scripts/jd_kingran_wind_cfd.js >> /scripts/logs/jd_kingran_wind_cfd.log 2>&118 0,18 * * * * * * node /scripts/jd_kingran_wind_cfd_help.js >> /scripts/logs/jd_kingran_wind_cfd_help.log 2>&1
-12 0,18 * * * node /scripts/jd_kingran_wind_cfd_help.js >> /scripts/logs/jd_kingran_wind_cfd.log 2>&1
+18 0-23/1 * * * node /scripts/jd_cfd.js >> /scripts/logs/jd_cfd.log 2>&118 0,18 * * * * * * node /scripts/jd_kingran_wind_cfd_help.js >> /scripts/logs/jd_kingran_wind_cfd_help.log 2>&1
 
 # 删除优惠券(默认注释，如需要自己开启，如有误删，已删除的券可以在回收站中还原，慎用)
 #20 9 * * 6 node /scripts/jd_delCoupon.js >> /scripts/logs/jd_delCoupon.log 2>&1
