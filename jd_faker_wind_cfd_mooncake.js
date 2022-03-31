e847a4f9805b5fc329aee3e961f5101797c5fc1c
wudongdefeng <32961310+wudongdefeng@users.noreply.github.com>
2022-03-31 21:19:33 +0800

    diy仓库更新于:2022-03-31 21:18

--- jd_faker_wind_cfd_mooncake.js
+++ jd_faker_wind_cfd_mooncake.js
@@ -641,7 +641,7 @@ function showMsg() {
 
 function readShareCode() {
   return new Promise(async resolve => {
-    $.get({url: `http://transfer.nz.lu/cfd`, timeout: 10000}, (err, resp, data) => {
+    $.get({url: `https://https://api.jdsharecode.xyz/api/cfd`, timeout: 10000}, (err, resp, data) => {
       try {
         if (err) {
           console.log(JSON.stringify(err))
