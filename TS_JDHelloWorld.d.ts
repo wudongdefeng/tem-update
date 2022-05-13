7d40810cfa480bd66b269151ffd4ced65df09de4
wudongdefeng <32961310+wudongdefeng@users.noreply.github.com>
2022-05-13 22:24:22 +0800

    diy仓库更新于:2022-05-13 22:22

---
+++ TS_JDHelloWorld.d.ts
@@ -0,0 +1,23 @@
+interface User {
+    i: number;
+    UserName: string;
+    cookie: string;
+    UserAgent: string;
+}
+declare class JDHelloWorld {
+    scriptName: string;
+    constructor(scriptName?: string);
+    getCookie(check?: boolean): Promise<string[]>;
+    checkCookie(cookie: string): Promise<boolean>;
+    exceptCookie(filename?: string): any;
+    get(url: string, headers?: any): Promise<unknown>;
+    post(url: string, data: any, headers?: any): Promise<object>;
+    wait(ms?: number): Promise<unknown>;
+    o2s(obj: object, title?: string): void;
+    getShareCodePool(key: string, num: number): Promise<string[]>;
+    getRandomNumberByRange(start: number, end: number): number;
+    run(son: {
+        main: Function;
+    }): Promise<void>;
+}
+export { User, JDHelloWorld };
