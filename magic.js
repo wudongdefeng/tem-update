f8ccef2ec605d2aad88c4be4614c1a815d9b7094
wudongdefeng <32961310+wudongdefeng@users.noreply.github.com>
2022-05-16 22:12:51 +0800

    diy仓库更新于:2022-05-16 22:10

--- magic.js
+++ magic.js
@@ -6,12 +6,14 @@ const {format} = require("date-fns");
 const notify = require('./sendNotify');
 const jdCookieNode = require('./jdCookie.js');
 const CryptoJS = require("crypto-js");
+const request = require('request');
 
 let cookies = [];
 let testMode = process.env.TEST_MODE?.includes('on') ? true
     : __dirname.includes("magic")
 
 let mode = process.env.MODE ? process.env.MODE : "local"
+let pandaToken=process.env.PandaToken?process.env.PandaToken:'';
 
 let wxBlackCookiePin = process.env.M_WX_BLACK_COOKIE_PIN
     ? process.env.M_WX_BLACK_COOKIE_PIN : ''
@@ -89,6 +91,36 @@ function uuid(x = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx") {
         return n.toString(36)
     })
 }
+
+function getSignfromPanda(functionId, body) {
+    var strsign = '';
+	let data = {
+      "fn":functionId,
+      "body": body
+    }
+    //console.log(JSON.stringify(data))
+    return new Promise(function(resolve) {
+            //var requestData = params
+            request({
+                url: "https://api.zhezhe.cf/jd/sign",
+                method: "POST",
+                json: true,
+                headers: {
+		        'Content-Type': 'application/json;charset=utf-8',
+		        'Authorization': 'Bearer '+pandaToken
+                },
+                body:data
+            }, function (error, response, body) {
+                console.log(body)
+                if (!error && response.statusCode == 200) {
+                    strsign={fn: body.data.fn, sign: body.data.sign};
+                    //console.log(strsign)
+                    resolve(strsign);
+                }
+                
+            });
+        })
+    }
 
 class Env {
     constructor(name) {
@@ -851,11 +883,13 @@ class Env {
 
     async sign(fn, body = {}) {
         let b = {"fn": fn, "body": body};
-        let h = {"key": "fMQ8sw1y5zF4RZgT"}
         try {
-            let {data} = await this.request(`http://140.238.59.174:19840/sign`,
-                h, b);
-            return {fn: data.fn, sign: data.body};
+            return getSignfromPanda(fn,body)
+        //let h = {"key": "fMQ8sw1y5zF4RZgT"}
+        //try {
+        //    let {data} = await this.request(`http://http://imagic.eu.org:17840/sign`,
+        //      h, b);
+        //    return {fn: data.fn, sign: data.body};
         } catch (e) {
             console.log("sign接口异常")
             //console.log("请自行配置sign实现")
