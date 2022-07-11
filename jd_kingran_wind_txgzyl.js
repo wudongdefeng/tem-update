2d8dee818b1f93c15c7ff98a85d6f10bcb430782
wudongdefeng <32961310+wudongdefeng@users.noreply.github.com>
2022-07-11 17:36:40 +0800

    diy仓库更新于:2022-07-11 17:35

--- jd_kingran_wind_txgzyl.js
+++ jd_kingran_wind_txgzyl.js
@@ -23,7 +23,7 @@ https://git.metauniverse-cn.com/https://raw.githubusercontent.com/curtinlv/gd/ma
 
 [task_local]
 #获取body后执行
-10 10 * * * https://git.metauniverse-cn.com/https://raw.githubusercontent.com/curtinlv/gd/main/jk_script/pkc_txgzyl.js, tag=PKC-特效关注有礼, enabled=false
+1 1 1 1 * https://git.metauniverse-cn.com/https://raw.githubusercontent.com/curtinlv/gd/main/jk_script/pkc_txgzyl.js, tag=PKC-特效关注有礼, enabled=false
 
 
 */
@@ -373,7 +373,12 @@ async function getMyPing(timeout = 500) {
                          $.log(`${JSON.stringify(err)}`);
                      }else {
                          $.data = JSON.parse(data);
-                         // console.log(JSON.stringify($.data,null, '\t'));
+												 rep_cookies = resp.headers['set-cookie'];
+												// console.log(rep_cookies);
+												r_cookie='';
+												for(var c in rep_cookies){
+														r_cookie += rep_cookies[c].split(" ")[0];
+												}
                           if($.data.result){
                               username=$.data.data.nickname;
                               pin=encodeURIComponent($.data.data.secretPin);
