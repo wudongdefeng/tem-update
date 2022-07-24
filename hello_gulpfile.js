3fbc9c3d067c3187819ddbdf51fac458faea2bf6
wudongdefeng <32961310+wudongdefeng@users.noreply.github.com>
2022-07-24 09:49:30 +0800

    diy仓库更新于:2022-07-24 09:47

--- hello_gulpfile.js
+++
@@ -1,7 +0,0 @@
-const gulp = require('gulp');
-const ts = require('gulp-typescript');
-const tsProject = ts.createProject('gulpconfig.json')
-
-gulp.task("default", () => {
-  return tsProject.src().pipe(tsProject()).js.pipe(gulp.dest('./dist'));
-})
