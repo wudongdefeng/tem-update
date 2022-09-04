05a7ef40c915b0b7ec519710399636fe498857d8
wudongdefeng <32961310+wudongdefeng@users.noreply.github.com>
2022-09-04 11:31:07 +0800

    diy仓库更新于:2022-09-04 11:29

--- magic.js
+++ magic.js
@@ -11,7 +11,7 @@ let isvObfuscatorRetryWait = parseInt(process.env?.M_WX_ISVOBFUSCATOR_RETRY_WAIT
 //模式
 let signMode = process.env.M_SIGN_MODE ? process.env.M_SIGN_MODE : "local"
 let apiToken = process.env.M_API_TOKEN ? process.env.M_API_TOKEN : ""
-let apiSignUrl = process.env.M_API_SIGN_URL ? process.env.M_API_SIGN_URL : "http://ailoveu.eu.org:19840/sign"
+let apiSignUrl = process.env.M_API_SIGN_URL ? process.env.M_API_SIGN_URL : "https://api.nolanstore.top/sign"
 let wskeyFile = process.env.M_WSKEY_FILE ? process.env.M_WSKEY_FILE : mode ? '/home/magic/Work/wools/doc/config.sh' : ''
 //无线pt_pin黑名单
 let blackPinConfig = {
