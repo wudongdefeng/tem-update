2f108568085c2832f4fa5f02abe501d00571b267
wudongdefeng <32961310+wudongdefeng@users.noreply.github.com>
2023-08-26 09:41:38 +0800

    diy仓库更新于:2023-08-26 09:41

--- jd_loadBlindBox.js
+++ jd_loadBlindBox.js
@@ -4,7 +4,6 @@
 环境变量：jd_loadBlindBox_activityUrl // 活动链接
 
 */
-let lnrun = 0;
 
 
 const $ = new Env('惊喜开盲盒（京耕）')
@@ -60,7 +59,6 @@ let domains = "https://" + $.domain;
       $.isLogin = true;
       $.nickName = "";
       console.log("\n******开始【京东账号" + $.index + "】" + ($.nickName || $.UserName) + "******\n");
-      lnrun++;if(lnrun == 4){console.log(`\n【访问接口次数达到3次，休息一分钟.....】\n`);await $.wait(60 * 1000);lnrun = 0}
       if (!$.isLogin) {
         $.msg($.name, "【提示】cookie已失效", "京东账号" + $.index + " " + ($.nickName || $.UserName) + "\n请重新登录获取\nhttps://bean.m.jd.com/", {
           "open-url": "https://bean.m.jd.com/"
