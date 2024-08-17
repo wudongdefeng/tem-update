c4fd9c3ca1189064554011aca0f024abd1a5acc0
wudongdefeng <32961310+wudongdefeng@users.noreply.github.com>
2024-08-17 20:41:02 +0800

    diy仓库更新于:2024-08-17 20:40

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -167,3 +167,4 @@
 3 3 29 2 * node /scripts/jd_drawShopGift.js >> /scripts/logs/jd_drawShopGift.log
 3 3 29 2 * node /scripts/jd_dplh0815.js >> /scripts/logs/jd_dplh0815.log
 3 3 29 2 * node /scripts/jd_evaluation.js >> /scripts/logs/jd_evaluation.log
+3 3 29 2 * node /scripts/jd_818.js >> /scripts/logs/jd_818.log
