f5ed82144e202756766aabb4f00bd2860977d437
wudongdefeng <32961310+wudongdefeng@users.noreply.github.com>
2024-09-13 00:41:08 +0800

    diy仓库更新于:2024-09-13 00:40

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -179,3 +179,4 @@
 3 3 29 2 * node /scripts/jd_dplh0908.js >> /scripts/logs/jd_dplh0908.log
 3 3 29 2 * node /scripts/jd_doudou.js >> /scripts/logs/jd_doudou.log
 3 3 29 2 * node /scripts/jd_dplh0910.js >> /scripts/logs/jd_dplh0910.log
+3 3 29 2 * node /scripts/jd_dplh0913.js >> /scripts/logs/jd_dplh0913.log
