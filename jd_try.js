44b2f439c3e255a1205781584fce90141e600f95
wudongdefeng <32961310+wudongdefeng@users.noreply.github.com>
2024-11-05 16:41:47 +0800

    diy仓库更新于:2024-11-05 16:41

--- jd_try.js
+++ jd_try.js
@@ -8,9 +8,7 @@ const axios = require('axios');
 const { SmashUtils } = require('./utils/smashUtils');
 const { setBaseCookie } = require('./utils/baseCookie');
 const { getJsToken } = require('./utils/jsToken.js');
-
 const cacheFilePath = path.join(__dirname, 'tokenCache.json');
-
 const $ = new Env('京东试用');
 const URL = 'https://api.m.jd.com/client.action';
 let trialActivityIdList = [];
@@ -82,7 +80,7 @@ $.innerKeyWords = [
 ];
 //下面很重要，遇到问题请把下面注释看一遍再来问
 let args_xh = {
-  h5st_server: process.env.H5ST_SERVER || 'https://jd.zhx47.top',
+  h5st_server: process.env.H5ST_SERVER || '',
   /*
    * 控制一次最多跑几个号，默认10个
    */
@@ -196,10 +194,13 @@ let args_xh = {
    * */
   sendNum: process.env.JD_TRY_SENDNUM * 1 || 4,
 };
-
 !(async () => {
   await $.wait(500);
-  $.log('\n遇到问题请先看脚本内注释；解决不了可联系https://t.me/zhouya47\n');
+  $.log('\n遇到问题请先看脚本内注释\n');
+  if(!args_xh.h5st_server) {
+    console.log('请先在环境变量中配置H5ST接口变量：export H5ST_SERVER="你的接口" 例子：export H5ST_SERVER="http://192.168.2.1:3111" (不要后面的/h5st)');
+    return;
+  }
   await requireConfig();
   if (!$.cookiesArr[0]) {
     console.log($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/');
@@ -356,7 +357,7 @@ async function try_apply(title, activityId) {
       },
       'x-api-eid-token': $.jsToken,
     },
-    '35fa0',
+    '20241024102328979;8v5eeh5lr1zk9er9;35fa0;tk03wc6231d3518nxa5SNv1G7xPveQBQ9ProecNhsUiJqMLXoJGTFmWAznDJpubNLB1gVxvi5YhTz3sB8_Xe8zgWthHs;280cd4752f7e2ee5a1e0cb41b512dff3;4.9;1729736608979;pjbMhjZd5WVdACUf56VeDWVS2TkdJrJdJrESJrpjh7Jj1jFfFmVd2XVf2HofIeYTImFf1rYdFq4eHeld4LoS6nFjLDIj7SnQEiVS0ipjLDrgJjIe4rIe7TYT5P4fJWod5T1TJiVd6nFS0nlSzjle6fFeJrJdJfUT1yVTIipjLDrgJnIgyzpe1uWS-GFSMWoRJrJdJTEjLrJp-jJO6XYZ0u1X-mYXjSEjLDIj_ulS9mFPJrpjh7JjGy1QDqWRJrJdJTlPJrpjh7ZMLrJp7rJdJLYOJipjLrpjh7JjJrJdJPYOJipjLrpjh7Jf5rJdJTYOJipjLrpjh7pfLDIj2XETJrpjLrJp-rojxjpe2iFjLrpjLDrg4Lojxj5f2iFjLrpjLDrg63pjxjJf2iFjLrpjLDrgJXIg6zpfMWlOC6FjLDIj6XETJrpjLrJp-rojxj5R0ipjLrpjh7pfLDIj46FjLrpjLDrg7rJdJ7FjLrpjLDrg7rJdJb1OJrpjLrJpwqJdJbFQGakNGipjLDrguqpjhjJhBDJhBDJjLDIj6rEjLrpjLD7NLDIj7qEjLrJp-jpVLf2YLfVTeqZSAGlQLT4U4nojYunjGy1QDqWRLXmXoq5dGy1QDqWRJrJdJnVO4ipjLD7N;c438f3c4dfb2e0b3d7d3c8a8ae4046b4',
   );
 
   setBaseCookie();
@@ -604,7 +605,7 @@ async function try_feedsList(tabId, page) {
       },
       'x-api-eid-token': $.jsToken,
     },
-    '35fa0',
+    '20241024102328979;8v5eeh5lr1zk9er9;35fa0;tk03wc6231d3518nxa5SNv1G7xPveQBQ9ProecNhsUiJqMLXoJGTFmWAznDJpubNLB1gVxvi5YhTz3sB8_Xe8zgWthHs;280cd4752f7e2ee5a1e0cb41b512dff3;4.9;1729736608979;pjbMhjZd5WVdACUf56VeDWVS2TkdJrJdJrESJrpjh7Jj1jFfFmVd2XVf2HofIeYTImFf1rYdFq4eHeld4LoS6nFjLDIj7SnQEiVS0ipjLDrgJjIe4rIe7TYT5P4fJWod5T1TJiVd6nFS0nlSzjle6fFeJrJdJfUT1yVTIipjLDrgJnIgyzpe1uWS-GFSMWoRJrJdJTEjLrJp-jJO6XYZ0u1X-mYXjSEjLDIj_ulS9mFPJrpjh7JjGy1QDqWRJrJdJTlPJrpjh7ZMLrJp7rJdJLYOJipjLrpjh7JjJrJdJPYOJipjLrpjh7Jf5rJdJTYOJipjLrpjh7pfLDIj2XETJrpjLrJp-rojxjpe2iFjLrpjLDrg4Lojxj5f2iFjLrpjLDrg63pjxjJf2iFjLrpjLDrgJXIg6zpfMWlOC6FjLDIj6XETJrpjLrJp-rojxj5R0ipjLrpjh7pfLDIj46FjLrpjLDrg7rJdJ7FjLrpjLDrg7rJdJb1OJrpjLrJpwqJdJbFQGakNGipjLDrguqpjhjJhBDJhBDJjLDIj6rEjLrpjLD7NLDIj7qEjLrJp-jpVLf2YLfVTeqZSAGlQLT4U4nojYunjGy1QDqWRLXmXoq5dGy1QDqWRJrJdJnVO4ipjLD7N;c438f3c4dfb2e0b3d7d3c8a8ae4046b4',
   );
 
   try {
@@ -729,7 +730,7 @@ async function try_feedsList(tabId, page) {
   }
 }
 
-async function h5stSign(body, appId, version = '4.9.1') {
+async function h5stSign(body, h5st, version = '4.9.1') {
   const options = {
     method: 'POST',
     url: `${args_xh.h5st_server}/h5st`,
@@ -739,10 +740,9 @@ async function h5stSign(body, appId, version = '4.9.1') {
       pin: $.UserName,
       ua: $.userAgent,
       body,
-      appId,
+      h5st,
     },
   };
-
   const { data } = await axios.request(options);
   return data.body;
 }
